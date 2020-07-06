1d3b47fda59e2f4b0daa206276604b745d432601
DigitalDave <dracman65@gmail.com>
2020-07-06 15:02:30 -0400

    Updated PS file

--- poat-inst-Chocolatey-1.ps1
+++ poat-inst-Chocolatey-1.ps1
@@ -1,5 +1,5 @@
 #Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Install Software
 choco install visualstudiocode -y
