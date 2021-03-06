cca4dee22c68a4c8927351b2a33554b92cd2d612
David Dracoules <dracman65@gmail.com>
2021-03-06 17:40:14 -0500

    Updated-SSH-VM

--- Azure-Bash-Install/azure-ubuntu-base.sh
+++ Azure-Bash-Install/azure-ubuntu-base.sh
@@ -8,4 +8,28 @@ az vmss create \
   --image UbuntuLTS \
   --upgrade-policy-mode automatic \
   --admin-username adminuser \
-  --generate-ssh-keys
+  --admin-password adminadmin123! \
+
+az vmss extension set \
+  --publisher Microsoft.Azure.Extensions \
+  --version 2.0 \
+  --name CustomScript \
+  --resource-group SS-RG-Test-1 \
+  --vmss-name myScaleSet \
+  --settings '{"fileUris":["https://raw.githubusercontent.com/Azure-Samples/compute-automation-configurations/master/automate_nginx.sh"],"commandToExecute":"./automate_nginx.sh"}'
+
+az network lb rule create \
+  --resource-group SS-RG-Test-1 \
+  --name TSTLoadBalancerRuleWeb \
+  --lb-name myScaleSetLB \
+  --backend-pool-name myScaleSetLBBEPool \
+  --backend-port 80 \
+  --frontend-ip-name loadBalancerFrontEnd \
+  --frontend-port 80 \
+  --protocol tcp
+
+az network public-ip show \
+  --resource-group SS-RG-Test-1 \
+  --name myScaleSetLBPublicIP \
+  --query '[ipAddress]' \
+  --output tsv
