ba46cef8e674e69bafa5c7d0cdaf2c0867502f50
David Dracoules <dracman65@gmail.com>
2021-03-27 15:03:05 -0400

    New BASH Scripts

---
+++ Azure-Bash-Install/Azure-Bash-Linux-MultiVM-Backend.sh
@@ -0,0 +1,169 @@
+az group create \
+--name RG-12-APPGW \
+--location centralus
+
+az network vnet create \
+--resource-group RG-12-APPGW \
+--name AZ104-vNET \
+--address-prefix 10.0.0.0/16 \
+--subnet-name APPGW-SUBNET \
+--subnet-prefix 10.0.0.0/24
+
+az network vnet subnet create \
+--address-prefixes 10.0.1.0/24 \
+--name BACKEND-SUBNET \
+--vnet-name AZ104-vNET \
+--resource-group RG-12-APPGW
+
+# NSG for VM-01
+az network nsg create \
+  --resource-group RG-12-APPGW \
+  --name NSG-VM-01
+
+az network nsg rule create \
+  --resource-group RG-12-APPGW \
+  --name NSG-VM-01-ALLOW-HTTP \
+  --nsg-name NSG-VM-01 \
+  --protocol tcp \
+  --direction inbound \
+  --source-address-prefix '*' \
+  --source-port-range '*' \
+  --destination-address-prefix 'VirtualNetwork' \
+  --destination-port-range 80 \
+  --access allow \
+  --priority 200
+
+az network nsg rule create \
+  --resource-group RG-12-APPGW \
+  --name NSG-VM-01-ALLOW-SSH \
+  --nsg-name NSG-VM-01\
+  --protocol tcp \
+  --direction inbound \
+  --source-address-prefix '*' \
+  --source-port-range '*' \
+  --destination-address-prefix 'VirtualNetwork' \
+  --destination-port-range 22 \
+  --access allow \
+  --priority 100
+
+# Deploy VM-01
+
+az vm create \
+  --resource-group RG-12-APPGW \
+  --name VM-01 \
+  --admin-username adminuser \
+  --admin-password adminadmin123! \
+  --image UbuntuLTS \
+  --vnet-name AZ104-vNET \
+  --nsg NSG-VM-01 \
+  --subnet BACKEND-SUBNET
+
+az vm extension set \
+  --publisher Microsoft.Azure.Extensions \
+  --version 2.0 \
+  --name CustomScript \
+  --vm-name VM-01 \
+  --resource-group RG-12-APPGW \
+  --settings '{"commandToExecute":"apt-get -y update && apt-get -y install apache2 && rm -rf /var/www/html && git clone https://github.com/XaaSTechnologies/AppGw-Default-Web-Page.github.io.git /var/www/html/"}'
+
+# NSG for VM-02
+az network nsg create \
+  --resource-group RG-12-APPGW \
+  --name NSG-VM-02
+
+az network nsg rule create \
+  --resource-group RG-12-APPGW \
+  --name NSG-VM-02-ALLOW-HTTP \
+  --nsg-name NSG-VM-02 \
+  --protocol tcp \
+  --direction inbound \
+  --source-address-prefix '*' \
+  --source-port-range '*' \
+  --destination-address-prefix 'VirtualNetwork' \
+  --destination-port-range 80 \
+  --access allow \
+  --priority 200
+
+az network nsg rule create \
+  --resource-group RG-12-APPGW \
+  --name NSG-VM-02-ALLOW-SSH \
+  --nsg-name NSG-VM-02\
+  --protocol tcp \
+  --direction inbound \
+  --source-address-prefix '*' \
+  --source-port-range '*' \
+  --destination-address-prefix 'VirtualNetwork' \
+  --destination-port-range 22 \
+  --access allow \
+  --priority 100
+
+# Deploy VM-02
+
+az vm create \
+  --resource-group RG-12-APPGW \
+  --name VM-02 \
+  --admin-username adminuser \
+  --admin-password adminadmin123! \
+  --image UbuntuLTS \
+  --vnet-name AZ104-vNET \
+  --nsg NSG-VM-02 \
+  --subnet BACKEND-SUBNET
+
+az vm extension set \
+  --publisher Microsoft.Azure.Extensions \
+  --version 2.0 \
+  --name CustomScript \
+  --vm-name VM-02 \
+  --resource-group RG-12-APPGW \
+  --settings '{"commandToExecute":"apt-get -y update && apt-get -y install apache2 && rm -rf /var/www/html && git clone https://github.com/XaaSTechnologies/AppGw-Images-Web-Server.github.io.git /var/www/html/images”}’
+
+# NSG for VM-03
+az network nsg create \
+  --resource-group RG-12-APPGW \
+  --name NSG-VM-03
+
+az network nsg rule create \
+  --resource-group RG-12-APPGW \
+  --name NSG-VM-03-ALLOW-HTTP \
+  --nsg-name NSG-VM-03 \
+  --protocol tcp \
+  --direction inbound \
+  --source-address-prefix '*' \
+  --source-port-range '*' \
+  --destination-address-prefix 'VirtualNetwork' \
+  --destination-port-range 80 \
+  --access allow \
+  --priority 200
+
+az network nsg rule create \
+  --resource-group RG-12-APPGW \
+  --name NSG-VM-03-ALLOW-SSH \
+  --nsg-name NSG-VM-03\
+  --protocol tcp \
+  --direction inbound \
+  --source-address-prefix '*' \
+  --source-port-range '*' \
+  --destination-address-prefix 'VirtualNetwork' \
+  --destination-port-range 22 \
+  --access allow \
+  --priority 100
+
+# Deploy VM-03
+
+az vm create \
+  --resource-group RG-12-APPGW \
+  --name VM-03 \
+  --admin-username adminuser \
+  --admin-password adminadmin123! \
+  --image UbuntuLTS \
+  --vnet-name AZ104-vNET \
+  --nsg NSG-VM-03 \
+  --subnet BACKEND-SUBNET
+
+az vm extension set \
+  --publisher Microsoft.Azure.Extensions \
+  --version 2.0 \
+  --name CustomScript \
+  --vm-name VM-03 \
+  --resource-group RG-12-APPGW \
+  --settings '{"commandToExecute":"apt-get -y update && apt-get -y install apache2 && rm -rf /var/www/html && git clone https://github.com/XaaSTechnologies/AppGw-Video-Web-Server.github.io.git /var/www/html/video”}’
---
+++ Azure-Bash-Install/Azure-Bash-x2VM-PVT-Network.sh
@@ -0,0 +1,89 @@
+az group create --name RG-13-Private-DNS --location centralus
+
+az network vnet create \
+  --name Private-DNS-vNET \
+  --resource-group RG-13-Private-DNS \
+  --location centralus \
+  --address-prefix 10.15.0.0/16 \
+  --subnet-name Subnet-01 \
+  --subnet-prefix 10.15.0.0/24
+
+az network nsg create \
+  --resource-group RG-13-Private-DNS \
+  --name NSG-VM-01
+
+az network nsg rule create \
+  --resource-group RG-13-Private-DNS \
+  --name NSG-VM-01-ALLOW-SSH \
+  --nsg-name NSG-VM-01 \
+  --protocol tcp \
+  --direction inbound \
+  --source-address-prefix '*' \
+  --source-port-range '*' \
+  --destination-address-prefix 'VirtualNetwork' \
+  --destination-port-range 22 \
+  --access allow \
+  --priority 100
+
+az network nsg rule create \
+  --resource-group RG-13-Private-DNS \
+  --name NSG-VM-01-ALLOW-ICMP \
+  --nsg-name NSG-VM-01 \
+  --protocol icmp \
+  --direction inbound \
+  --source-address-prefix '*' \
+  --source-port-range '*' \
+  --destination-address-prefix 'VirtualNetwork' \
+  --destination-port-range '*' \
+  --access allow \
+  --priority 200
+
+az vm create \
+  --resource-group RG-13-Private-DNS \
+  --name VM-01 \
+  --admin-username adminuser \
+  --admin-password adminadmin123! \
+  --image UbuntuLTS \
+  --vnet-name Private-DNS-vNET \
+  --nsg NSG-VM-01 \
+  --subnet Subnet-01
+
+az network nsg create \
+  --resource-group RG-13-Private-DNS \
+  --name NSG-VM-02
+
+az network nsg rule create \
+  --resource-group RG-13-Private-DNS \
+  --name NSG-VM-02-ALLOW-SSH \
+  --nsg-name NSG-VM-02 \
+  --protocol tcp \
+  --direction inbound \
+  --source-address-prefix '*' \
+  --source-port-range '*' \
+  --destination-address-prefix 'VirtualNetwork' \
+  --destination-port-range 22 \
+  --access allow \
+  --priority 100
+
+az network nsg rule create \
+  --resource-group RG-13-Private-DNS \
+  --name NSG-VM-02-ALLOW-ICMP \
+  --nsg-name NSG-VM-02 \
+  --protocol icmp \
+  --direction inbound \
+  --source-address-prefix '*' \
+  --source-port-range '*' \
+  --destination-address-prefix 'VirtualNetwork' \
+  --destination-port-range '*' \
+  --access allow \
+  --priority 200
+
+az vm create \
+  --resource-group RG-13-Private-DNS \
+  --name VM-02 \
+  --admin-username adminuser \
+  --admin-password adminadmin123! \
+  --image UbuntuLTS \
+  --vnet-name Private-DNS-vNET \
+  --nsg NSG-VM-02 \
+  --subnet Subnet-01
